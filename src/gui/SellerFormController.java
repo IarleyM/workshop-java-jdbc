4fc1458c48546d13943c74c323bbb57a29825698
IarleyM <iarleym771@gmail.com>
2023-03-07 20:07:49 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -119,7 +121,26 @@ public class SellerFormController implements Initializable {
 			exception.addError("name", "Field can´t be empty");
 		}
 		obj.setName(txtName.getText());
+		
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can´t be empty");
+		}
+		obj.setEmail(txtEmail.getText());
 
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can´t be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can´t be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+
+		obj.setDepartment(comboBoxDepartment.getValue());
+		
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
@@ -171,9 +192,9 @@ public class SellerFormController implements Initializable {
 		if (entity.getBaseSalary() != null) {
 			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
 		}
-		if(entity.getDepartment() == null) {
+		if (entity.getDepartment() == null) {
 			comboBoxDepartment.getSelectionModel().selectFirst();
-		}else {
+		} else {
 			comboBoxDepartment.setValue(entity.getDepartment());
 		}
 	}
@@ -189,10 +210,15 @@ public class SellerFormController implements Initializable {
 
 	private void setErrorMessages(Map<String, String> error) {
 		Set<String> fields = error.keySet();
-
-		if (fields.contains("name")) {
-			labelErrorName.setText(error.get("name"));
-		}
+		
+		labelErrorName.setText((fields.contains("name"))?error.get("name") :"");
+		
+		labelErrorEmail.setText((fields.contains("email"))?error.get("email") :"");
+		
+		labelErrorBirthDate.setText((fields.contains("birthDate"))?error.get("birthDate") :"");
+		
+		labelErrorBaseSalary.setText((fields.contains("baseSalary"))?error.get("baseSalary") :"");
+		
 	}
 
 	private void initializeComboBoxDepartment() {
